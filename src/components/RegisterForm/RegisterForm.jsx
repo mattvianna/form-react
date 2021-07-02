743b8d399ad8ed70f4ce4e4f030974ede0c60b28
Matheus Vianna <matheus.vianna@agenciam3.com>
2021-07-02 11:50:51 -0300

    implementação da lib Material UI com react e inicio da componetização do form

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
+
+function RegisterForm() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome, sobrenome);
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          setNome(event.target.value);
+        }}
+        id="nome"
+        label="Nome"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+        id="sobrenome"
+        label="Sobrenome"
+        margin="normal"
+        fullWidth
+      />
+
+      <TextField id="cpf" label="CPF" margin="normal" fullWidth />
+
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultChecked color="primary" />}
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
+      />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default RegisterForm;
